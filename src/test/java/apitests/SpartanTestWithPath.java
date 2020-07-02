6b717fc4b06a1aa65570c72f8825ac9102ee0939
CybertekSchool <github@cybertekschool.com>
2020-07-02 15:27:19 -0400

    conf added

---
+++ src/test/java/apitests/SpartanTestWithPath.java
@@ -0,0 +1,26 @@
+package apitests;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import static org.testng.Assert.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SpartanTestWithPath {
+
+
+    @BeforeClass
+    public void beforeClass(){
+        RestAssured.baseURI = "http://3.81.99.109:8000";
+    }
+
+}
