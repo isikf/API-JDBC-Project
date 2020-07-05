fdfe5c2fd5e697c6092b4e1d8939cc357f06a404
CybertekSchool <github@cybertekschool.com>
2020-07-05 12:17:12 -0400

    day4 jsonpath

---
+++ src/test/java/apitests/SpartanTestWithJsonPath.java
@@ -0,0 +1,37 @@
+package apitests;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import static org.testng.Assert.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+public class SpartanTestWithJsonPath {
+
+    @BeforeClass
+    public void beforeClass(){
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartanapi_url");
+    }
+
+
+        /*
+          Given accept type is json
+          And path param spartan id is 11
+          When user sends a get request to /spartans/{id}
+         Then status code is 200
+         And content type is Json
+         And   "id": 11,
+               "name": "Nona",
+              "gender": "Female",
+              "phone": 7959094216
+    */
+}
