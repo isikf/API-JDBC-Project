c8940425c8d65662752c66463a217feaa55fba0b
CybertekSchool <github@cybertekschool.com>
2020-07-07 15:37:08 -0400

    day5 gson added

---
+++ src/test/java/apitests/JsonToJavaCollection.java
@@ -0,0 +1,46 @@
+package apitests;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
+import utilities.ConfigurationReader;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import static org.testng.Assert.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+
+import java.util.Map;
+
+public class JsonToJavaCollection {
+
+    @BeforeClass
+    public void beforeClass(){
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartanapi_url");
+    }
+
+
+    @Test
+    public void SpartanToMap(){
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 15)
+                .and().auth().basic("admin", "admin")
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(response.statusCode(),200);
+
+        //we will convert Json Response to Java Collection
+        Map<String,Object> spartanMap = response.body().as(Map.class);
+
+    }
+}
