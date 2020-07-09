4ab1f002a38517d37a0d68ff5ca3d7ccba7dd66b
CybertekSchool <github@cybertekschool.com>
2020-07-09 13:58:30 -0400

    day 7 post

--- src/test/java/Day6/PostRequestDemo.java
+++ src/test/java/Day6/PostRequestDemo.java
@@ -19,6 +19,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ConfigurationReader;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,7 +37,7 @@ public class PostRequestDemo {
       "name":"MikeEU",
       "phone":8877445596
    }
-    When user sends POST request to '/spartans/'
+    When user sends POST request to '/api/spartans'
     Then status code 201
     And content type should be application/json
     And json payload/response should contain:
@@ -46,9 +47,131 @@ public class PostRequestDemo {
 
     @Test
     public void PostNewSpartan(){
+        String jsonBody ="{\n" +
+                "      \"gender\":\"Male\",\n" +
+                "      \"name\":\"MikeEU\",\n" +
+                "      \"phone\":8877445596\n" +
+                "   }";
 
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().auth().basic("admin", "admin")
+                .and().body(jsonBody)
+                .when().post("/api/spartans");
 
+        //verify status code
+        assertEquals(response.statusCode(),201);
+        //verify content type
+        assertEquals(response.contentType(),"application/json");
 
+        //verify message
+        String message = response.path("success");
+        assertEquals(message,"A Spartan is Born!");
+
+        String name = response.path("data.name");
+        String gender = response.path("data.gender");
+        long phone =response.path("data.phone");
+
+        //assertion
+        assertEquals(name,"MikeEU");
+        assertEquals(gender,"Male");
+        assertEquals(phone,8877445596l);
 
     }
+
+    @Test
+    public void PostNewSpartan2(){
+        //create map to keep request json body info
+        Map<String,Object> requestMap = new HashMap<>();
+        //adding values that we want to post
+        requestMap.put("name","MikeEU");
+        requestMap.put("gender","Male");
+        requestMap.put("phone",8877445596l);
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().auth().basic("admin", "admin")
+                .and().body(requestMap)
+                .when().post("/api/spartans");
+
+        //verify status code
+        assertEquals(response.statusCode(),201);
+        //verify content type
+        assertEquals(response.contentType(),"application/json");
+
+        //verify message
+        String message = response.path("success");
+        assertEquals(message,"A Spartan is Born!");
+
+        String name = response.path("data.name");
+        String gender = response.path("data.gender");
+        long phone =response.path("data.phone");
+
+        //assertion
+        assertEquals(name,"MikeEU");
+        assertEquals(gender,"Male");
+        assertEquals(phone,8877445596l);
+
+        int idFromPOST = response.path("data.id");
+        //===========END OF POST REQUEST
+        //after post the new spartan, we want to send get request
+         given().pathParam("id",idFromPOST)
+                 .and().auth().basic("admin","admin")
+                 .and().accept(ContentType.JSON)
+                 .when().get("/api/spartans/{id}")
+                 .then().assertThat().statusCode(200).log().all();
+    }
+
+    @Test
+    public void PostNewSpartan3(){
+        //get spartan info from mackaroo and send post requests(optional homework)
+        //-Create one mackaroo api for name,gender,phone
+        //send get request to retrieve random info from api
+        //use those info to send post request to spartan
+
+
+        //create spartan object to send information
+        Spartan spartanEU = new Spartan();
+        spartanEU.setName("MikeEU");
+        spartanEU.setGender("Male");
+        spartanEU.setPhone(8877445596l);
+
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().auth().basic("admin", "admin")
+                .and().body(spartanEU)
+                .when().post("/api/spartans");
+
+        //verify status code
+        assertEquals(response.statusCode(),201);
+        //verify content type
+        assertEquals(response.contentType(),"application/json");
+
+        //verify message
+        String message = response.path("success");
+        assertEquals(message,"A Spartan is Born!");
+
+        String name = response.path("data.name");
+        String gender = response.path("data.gender");
+        long phone =response.path("data.phone");
+
+        //assertion
+        assertEquals(name,spartanEU.getName());
+        assertEquals(gender,spartanEU.getGender());
+        assertEquals(phone,spartanEU.getPhone());
+
+        int idFromPOST = response.path("data.id");
+        //===========END OF POST REQUEST
+        //after post the new spartan, we want to send get request
+        given().pathParam("id",idFromPOST)
+                .and().auth().basic("admin","admin")
+                .and().accept(ContentType.JSON)
+                .when().get("/api/spartans/{id}")
+                .then().assertThat().statusCode(200).log().all();
+    }
+
+
+
+
 }
